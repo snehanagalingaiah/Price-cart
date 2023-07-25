03cc60dd1a939fc3e408d9455e8a60103891b779
snehanagalingaiah <snehanagalingaiah@gmail.com>
2023-07-25 16:48:42 +0530

    making cards responsive

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <div className="App">   
      <Cart />
      <Banner /> 
-        <div className="row row-cols-1 row-cols-md-4 g-4">
+        <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 g-4">
        {
          cardData.map(card => <Card card={card} />)
        } 
